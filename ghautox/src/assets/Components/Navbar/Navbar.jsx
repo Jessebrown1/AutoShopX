f3a5b2c69d847e6a4e31219a96565f409ccfdac2
Jesse Brown <jnbentum@gmail.com>
2025-08-15 13:05:31 +0000

    updated search bar

--- ghautox/src/assets/Components/Navbar/Navbar.jsx
+++ ghautox/src/assets/Components/Navbar/Navbar.jsx
@@ -13,11 +13,42 @@
                 <ul className='hidden md:flex gap-7 text-white'>
                     <a href="#Header" className='cursor-pointer hover:text-gray-400'>Browse Parts</a>
                     <a href="#About" className='cursor-pointer hover:text-gray-400'>Sell Parts</a>
-                    <a href="#Contact" className='cursor-pointer hover:text-gray-400'>Help</a>
+                    <a href="#Contact" className='cursor-pointer hover:text-gray-400'>Mechanics</a>
                 </ul>
                 
 
-<input type="text" name="text" class="input" placeholder="Search For Parts...."></input>
+
+                
+                                    <div className="relative">
+                    <input
+                        placeholder="Search..."
+                        name="search"
+                        type="search"
+                        className="shadow-lg border border-gray-300 px-4 py-2 rounded-xl w-48 md:w-56 transition-all duration-300 focus:w-64 outline-none text-sm bg-gray-100 focus:border-2 focus:border-[#009087]"
+                    />
+                    <svg
+                        className="w-5 h-5 absolute top-2.5 right-3 text-gray-500 pointer-events-none"
+                        stroke="currentColor"
+                        strokeWidth="1.5"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                        d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                        strokeLinejoin="round"
+                        strokeLinecap="round"
+                        ></path>
+                    </svg>
+                    </div>
+
+
+
+
+
+
+
+
                 <button className='hidden md:block bg-white px-8 py-2 rounded-full'>Login</button>
                 <img onClick={()=> setShowMobileMenu(true)} src={menu} alt="Menu Icon" className='md:hidden w-7 cursor-pointer ' />
             </div>
